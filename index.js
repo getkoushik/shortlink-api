43d44e83f7b37aed6ba8ee6669a11572822a8bdb
respecting <62519659+respecting@users.noreply.github.com>
2020-11-26 12:04:58 -0700

    ip logger detection

--- index.js
+++ index.js
@@ -10,12 +10,52 @@ const express = require('express')
       windowMs: 15 * 60 * 1000,
       max: 100,
       message: "This ip has sent too many requests."
-    })
+    }),
+    ipLoggers = [
+        "viral.over-blog.com",
+        "gyazo.in",
+        "ps3cfw.com",
+        "urlz.fr",
+        "webpanel.space",
+        "steamcommumity.com",
+        "imgur.com.de",
+        "fuglekos.com",
+        "grabify.link",
+        "leancoding.co",
+        "stopify.co",
+        "freegiftcards.co",
+        "joinmy.site",
+        "curiouscat.club",
+        "catsnthings.fun",
+        "catsnthings.com",
+        "xn--yutube-iqc.com",
+        "gyazo.nl",
+        "yip.su",
+        "iplogger.com",
+        "iplogger.org",
+        "iplogger.ru",
+        "2no.co",
+        "02ip.ru",
+        "iplis.ru",
+        "iplo.ru",
+        "ezstat.ru",
+        "whatstheirip.com",
+        "hondachat.com",
+        "bvog.com",
+        "youramonkey.com",
+        "pronosparadise.com",
+        "freebooter.pro",
+        "blasze.com",
+        "blasze.tk",
+        "ipgrab.org",
+        "gyazos.com",
+        "discord.kim"
+    ];
 
 app.set('trust proxy', 1);
 
 app.get('/', (req, res) => {
-  res.send('<html><head><style>body {text-align:center;font-family:Arial;color:white;background:#000;}</style></head><body><h1>Shortlink Revealer API</h1><h2>Usage:</h2><p>https://api.kashooting.tech/api?url=shortlink</p><br><br><a href="https://github.com/respecting/shortlink-api">Fork me on Github!</a></body></html>')
+  res.send('<html><head><style>body {text-align:center;font-family:Arial;color:white;background:#000;}</style></head><body><h1>Shortlink Revealer API</h1><h2>Usage:</h2><p>https://api.kashooting.tech/api?url=linkvertiseurl</p><br><br><a href="https://github.com/respecting/shortlink-api">Fork me on Github!</a></body></html>')
 })
 
 app.use('/api', limit)
@@ -23,6 +63,7 @@ app.use('/api', limit)
 function validateUrl(url, res) {
   try {
         let myURL = new URL(url);
+        if(ipLoggers.includes(myURL.host)) return res.status(403).json({success: false, err:"IP Logger detected."});
         bypass(myURL, res);
     } catch (e) {
       res.status(400).json({success: false, err:"Not a valid link."})
